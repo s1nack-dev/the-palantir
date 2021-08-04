eb13e105ec323edf5afc5ff6a89898dfcd6be37a
Sean Ryan <seanryan23@gmail.com>
2021-08-03 20:13:16 -0700

    added services

--- camera.py
+++ camera.py
@@ -1,1 +1,15 @@
 # https://picamera.readthedocs.io/en/release-1.13/index.html
+
+# https://www.youtube.com/watch?v=bWwZF_zVf00
+
+# sudo apt-get install python-picamera python3-picamera
+# sudo apt-get update && sudo apt-get upgrade -y && sudo reboot
+# sudo rpi-update
+# # sudo reboot
+# sudo apt-get install motion
+# sudo modprobe bcm2835-v4l2
+
+# https://projects-raspberry.com/live-camera-streaming-raspberry-pi-motion-setup/
+
+# have to run this, otherwise it tries to use the init.d service, which causes issues possibly 
+#sudo cp /usr/share/motion/examples/motion.service /etc/systemd/system/
--- cellular.py
+++ cellular.py
@@ -2,3 +2,24 @@
 # Can also trigger disable hostpot wifi script
 # Use tmobile card instead
 
+
+# https://toddhayton.com/2014/12/08/form-handling-with-mechanize-and-beautifulsoup/
+
+
+
+# POST /goform/goform_set_cmd_process HTTP/1.1
+# Host: 192.168.0.1
+# Content-Length: 59
+# Accept: application/json, text/javascript, */*; q=0.01
+# X-Requested-With: XMLHttpRequest
+# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36
+# Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+# Origin: http://192.168.0.1
+# Referer: http://192.168.0.1/index.html
+# Accept-Encoding: gzip, deflate
+# Accept-Language: en-US,en;q=0.9
+# Connection: close
+
+# isTest=false&goformId=LOGIN&password=YWRtaW4%3D&save_login=
+
+from bs4 import BeautifulSoup
